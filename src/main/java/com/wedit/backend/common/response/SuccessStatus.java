f833ef6efc93e1fe05236ee79c153d390cbcd68a
Hyunbin Kim <taque88@naver.com>
2026-02-25 03:09:18 +0900

    [feat] S3 관련 성공/실패 메시지 추가

--- src/main/java/com/wedit/backend/common/response/SuccessStatus.java
+++ src/main/java/com/wedit/backend/common/response/SuccessStatus.java
@@ -11,15 +11,18 @@ public enum SuccessStatus {
 
     /// 200 OK
     FORM_LOGIN_SUCCESS(HttpStatus.OK, "폼 로그인 성공"),
+    MEMBER_WITHDRAW_SUCCESS(HttpStatus.OK, "회원탈퇴 성공"),
+    TOKEN_REISSUE_SUCCESS(HttpStatus.OK, "토큰 재발급 성공"),
+    MEDIA_LIST_SUCCESS(HttpStatus.OK, "미디어 목록 조회 성공"),
+    MEDIA_UPDATE_SUCCESS(HttpStatus.OK, "미디어 수정 성공"),
+    AWS_S3_PRESIGNED_URL_SUCCESS(HttpStatus.OK, "S3 Presigned URL 발급 성공"),
 
     /// 201 CREATED
     MEMBER_SIGNUP_SUCCESS(HttpStatus.CREATED, "회원가입 성공"),
+    MEDIA_ADD_SUCCESS(HttpStatus.CREATED, "미디어 추가 성공"),
 
-    /// 200 OK
-    MEMBER_WITHDRAW_SUCCESS(HttpStatus.OK, "회원탈퇴 성공"),
-
-    /// 200 OK
-    TOKEN_REISSUE_SUCCESS(HttpStatus.OK, "토큰 재발급 성공"),
+    /// 204 NO CONTENT
+    MEDIA_DELETE_SUCCESS(HttpStatus.NO_CONTENT, "미디어 삭제 성공"),
 
 
 
